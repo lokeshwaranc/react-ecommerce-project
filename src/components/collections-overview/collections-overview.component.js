81ac37864b7f2c455ca093ad7adee264aa599556
dev <lokeshwaran.c@gmail.com>
2020-10-26 18:42:06 +0530

    added middleware redux-thunk to shop reducer, added container pattern to collection and collection-overview

--- src/components/collections-overview/collections-overview.component.js
+++ src/components/collections-overview/collections-overview.component.js
@@ -2,10 +2,10 @@ import React from 'react';
 import {connect} from 'react-redux'; 
 import {createStructuredSelector} from 'reselect'
 
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+import CollectionPreview from '../collection-preview/collection-preview.component';
 import {selectCollectionsForPreview} from '../../redux/shop/shop.selectors';
 
-import './collection-overview.style.scss';
+import './collections-overview.style.scss';
 
 const CollectionsOveriew = ({collections}) => (
 	<div className='collections-overview'>
